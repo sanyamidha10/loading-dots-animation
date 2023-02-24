808e85662f2cf017d075cf1d188c8a0b599d2b3a
Sanya Midha <sanyamidha.10@gmail.com>
2023-02-24 21:34:05 +0530

    Loading dots with setTimeout function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 /*=============== ANIMATED DOTS JS ===============*/
+const dots = document.querySelector('.loading__dots')
+
+const addAnimate = () =>{
+    // Add Animate class
+    dots.classList.add('animate')
+
+    setTimeout(() =>{
+        // Remove animate class
+        dots.classList.remove('animate')
+
+        setTimeout(() =>{
+            addAnimate()
+        }, 100)
+
+    },2600)
+}
+
+addAnimate()
